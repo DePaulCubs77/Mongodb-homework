b43aacd53b39e4b9d411da06cda1d3ea47170cea
Maria  Garcia <MariaGarcia@Marias-MacBook-Pro.local>
2017-02-25 02:34:55 -0600

    Still in Progress

---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies 
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+
+// Require request and cheerio. This makes scraping possible.
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Initialize Express 
+var app = express();
+
+
+
+
